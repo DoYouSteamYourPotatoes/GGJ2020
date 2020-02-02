6b2ef107c3c6b39b7427df5de432086480eb3a7b
Teiki Guilloux <guilloux.teiki@gmail.com>
2020-02-02 06:47:42 -0500

    Checkpoint + CheckpointManagerLogic

---
+++ GGJ2020Project/Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    private Checkpoint currentActiveCheckpoint;
+
+    public Checkpoint CurrentActiveCheckpoint
+    {
+        get => currentActiveCheckpoint;
+        set
+        {
+            if (currentActiveCheckpoint != null && value != currentActiveCheckpoint)
+            {
+                currentActiveCheckpoint.Active = false;
+            }
+            currentActiveCheckpoint = value;
+        }
+    }
+
+    public void Respawn()
+    {
+
+    }
+}
